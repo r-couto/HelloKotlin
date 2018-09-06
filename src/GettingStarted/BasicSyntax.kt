50a4b5069c244c6cd6bb7fedfb6850353ed4a2fc
Ricardo <ricoutos@gmail.com>
2018-09-05 23:12:31 -0300

    test: Basic Syntax 2

--- src/GettingStarted/BasicSyntax.kt
+++ src/GettingStarted/BasicSyntax.kt
@@ -6,20 +6,47 @@ class BasicSyntax : Exercicio {
 
     override fun executar() {
 //        e1();
-        e2();
+//        e2();
+//        e3();
+        e4();
     }
 
     fun e1(): Unit {
         println("HelloKotlin")
     }
     fun e2(): Unit {
-        val a = 5
-        val b = 6
-        val resultado = soma(a, b)
-        println("${a} + ${b} = ${resultado}")
+        val a = 11
+        val b = 25
+        println("$a + $b = ${soma(a, b)}")
+//        val resultado = soma(a, b)
+//        println("${a} + ${b} = ${resultado}")
     }
 
-    fun soma(a: Int, b: Int): Int {
-        return a + b
+    fun e3(): Unit {
+        val a = 11
+        val b = 25
+        println("Maior entre $a e $b = ${maior(a, b)}")
     }
+
+    fun e4(): Unit {
+        val a = null
+        val b = 25
+        println("${if(a == null) 0 else a} + $b = ${soma2(a, b)}")
+    }
+
+    fun soma(a: Int, b: Int) = a + b
+    /*fun soma(a: Int, b: Int): Int {
+        return a + b
+    }*/
+
+    fun maior(a: Int, b: Int) = if (a > b) a else b
+
+    fun soma2(a: Int?, b: Int): Int {
+        if (a != null) {
+            return a + b
+        } else {
+            return b
+        }
+    }
+
 }
